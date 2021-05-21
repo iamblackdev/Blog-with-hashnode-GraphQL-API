c32256da56237a63bc49d946b000ad62bfc99c14
iamwahabhabib <wahabhabib23@gmail.com>
2021-05-21 01:19:33 +0100

    fix bug

--- src/custom hooks/useFetch.js
+++ src/custom hooks/useFetch.js
@@ -108,14 +108,9 @@ const useFetch = (username, slug, filter) => {
   useEffect(() => {
     // setIsPending(false)
     setSpin(true)
-    // if ( (localStorage.getItem('datas')) !== null && (username == JSON.parse(localStorage.getItem('datas')).data.user.username) && (JSON.parse(localStorage.getItem('datas')).data.post !== null ) && (slug == JSON.parse(localStorage.getItem('datas')).data.post.slug)) {
-    //   setDatas(JSON.parse(localStorage.getItem('datas')))
-    //   setIsPending(true)
-    //   setSpin(false)
-    //   setError(null)
-    // } else {
+   
       fetchData()
-    // }   
+   
   }, [username, slug, filter])
 
   return {datas, isPending, error, spin}
